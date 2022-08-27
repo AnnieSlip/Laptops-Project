751427ee8f9c82d124813df180667d9f2608985a
AnnieSlip <javakhishvili.an@gmail.com>
2022-08-27 11:16:02 +0400

    Start working on second page

---
+++ script.js
@@ -0,0 +1,7 @@
+const damateba = document.getElementById("damateba");
+const sia = document.getElementById("sia");
+
+damateba.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location = "/pages/tanamshromlis-info.html";
+});
