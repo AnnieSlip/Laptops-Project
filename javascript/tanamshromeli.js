6ef76b379dc15ff6dffa4ebafae650b52b83c7d8
AnnieSlip <javakhishvili.an@gmail.com>
2022-08-28 23:58:51 +0400

    TANAMSHROMELI PAGE FORM VALIDATION

--- javascript/tanamshromeli.js
+++ javascript/tanamshromeli.js
@@ -2,14 +2,19 @@ const teamDropdown = document.querySelector(".team");
 const positionDropdown = document.querySelector("#position");
 const teamContainer = document.querySelector(".team-container");
 const positionContainer = document.querySelector(".position-container");
+const form = document.querySelector(".form-container");
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 
 //Get Teams data from api
 const getTeam = async function () {
   try {
     const res = await fetch("https://pcfy.redberryinternship.ge/api/teams");
     const data = await res.json();
-    const teamsList = data.data;
-    console.log(teamsList);
+    const teamsList = data.data.reverse();
+    //console.log(teamsList);
     teamsList.map((team) => {
       renderData(teamContainer, team.id, team.name);
       //teamValue = team.id;
@@ -45,20 +50,106 @@ function getSelectValue() {
       const data = await res.json();
 
       const positionList = data.data;
-      console.log(positionList);
+      //console.log(positionList);
 
       positionList.map((elem) => {
         if (elem.team_id == selectValue) {
           renderData(positionContainer, elem.id, elem.name);
-          console.log(elem);
+          //console.log(elem);
         }
       });
     } catch (err) {
       console.log(err);
     }
   };
-
   getPosition();
 }
 
-//const value = document.getElementById("team").value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateForm();
+});
+
+const validateForm = function () {
+  const team = document.getElementById("team");
+
+  const position = document.getElementById("position");
+
+  //Firstname
+  if (firstName.value.trim() == "") {
+    setError(firstName, "ველი არ უნდა იყოს ცარიელი");
+  } else if (firstName.value.trim().length < 3) {
+    setError(firstName, "მინიმუმ 2 სიმბოლო");
+  } else {
+    setSucces(firstName, "მინიმუმ 2 სიმბოლო, ქართული ასოები");
+  }
+  //Lastname
+  if (lastName.value.trim() == "") {
+    setError(lastName, "ველი არ უნდა იყოს ცარიელი");
+  } else if (lastName.value.trim().length < 3) {
+    setError(lastName, "მინიმუმ 2 სიმბოლო");
+  } else {
+    setSucces(lastName, "მინიმუმ 2 სიმბოლო, ქართული ასოები");
+  }
+
+  //Email
+  if (email.value.trim() == "") {
+    setError(email, "ველი არ უნდა იყოს ცარიელი");
+  } else if (isEmailValid(email.value)) {
+    setSucces(email, "უნდა მთავრდებოდეს @redberry.ge");
+  } else {
+    setError(email, "უნდა მთავრდებოდეს @redberry.ge");
+  }
+  //Phone
+  if ((phone.value = "")) {
+    setError(phone, "ველი არ უნდა იყოს ცარიელი");
+  } else if ((phone.value = "+995")) {
+    setSucces(phone, "უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს");
+  } else {
+    setError(phone, "უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს");
+  }
+  //team
+  if (team.value == "") {
+    setError(team);
+  } else {
+    setSucces(team);
+  }
+  //position
+  if (position.value == "") {
+    setError(position);
+  } else {
+    setSucces(position);
+  }
+};
+const setError = function (element, errorMessage) {
+  const parent = element.parentElement;
+  const paragraph = parent.querySelector("p");
+
+  if (parent.classList.contains("success")) {
+    parent.classList.remove("success");
+  }
+
+  parent.classList.add("error");
+
+  if (paragraph) {
+    paragraph.textContent = errorMessage;
+  }
+};
+
+const setSucces = function (element, message) {
+  const parent = element.parentElement;
+  const paragraph = parent.querySelector("p");
+  if (parent.classList.contains("error")) {
+    parent.classList.remove("error");
+    if (paragraph) {
+      paragraph.textContent = message;
+    }
+  }
+
+  parent.classList.add("success");
+};
+
+const isEmailValid = function (email) {
+  const reg = /^([a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+)@redberry.ge$/;
+  return reg.test(email);
+};
