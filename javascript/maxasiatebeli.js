5d0260de18cadc1dd435767a9ab411c332a5214f
AnnieSlip <javakhishvili.an@gmail.com>
2022-08-29 23:46:52 +0400

    STYLING FILE-UPLOAD

--- javascript/maxasiatebeli.js
+++ javascript/maxasiatebeli.js
@@ -1,6 +1,24 @@
 "use strict";
+const image_input = document.querySelector("#image_input");
+let uploaded_image = "";
+const realFileBtn = document.getElementById("image_input");
+const custumBtn = document.getElementById("custom-button");
 
 const params = new URLSearchParams(window.location.search);
 params.forEach((value, key) => {
   console.log(value, key);
+});
+image_input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploaded_image = reader.result;
+    document.querySelector(
+      "#display_image"
+    ).style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+custumBtn.addEventListener("click", function () {
+  realFileBtn.click();
 });
