ed849aa55f9bba0faf1a68d4ca75282b701d0420
AnnieSlip <javakhishvili.an@gmail.com>
2022-08-30 10:10:56 +0400

    GET LAPTOPBRAND AND CPU DATA FROM API AND RENDER

--- javascript/maxasiatebeli.js
+++ javascript/maxasiatebeli.js
@@ -3,11 +3,14 @@ const image_input = document.querySelector("#image_input");
 let uploaded_image = "";
 const realFileBtn = document.getElementById("image_input");
 const custumBtn = document.getElementById("custom-button");
-
+const laptopBrandContainer = document.querySelector(".brand-container");
+const laptopCpuContainer = document.querySelector(".cpu-container");
+//Recieve data from previous page
 const params = new URLSearchParams(window.location.search);
 params.forEach((value, key) => {
   console.log(value, key);
 });
+//File upload and display
 image_input.addEventListener("change", function () {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
@@ -18,7 +21,42 @@ image_input.addEventListener("change", function () {
   });
   reader.readAsDataURL(this.files[0]);
 });
-
+//Custom radio buttons click
 custumBtn.addEventListener("click", function () {
   realFileBtn.click();
 });
+//get LaptopBrand data from api
+const getLaptopBrands = async function () {
+  try {
+    const res = await fetch("https://pcfy.redberryinternship.ge/api/brands");
+    const data = await res.json();
+    const laptopBrands = data.data.reverse();
+    laptopBrands.map((brand) => {
+      renderData(laptopBrandContainer, brand.id, brand.name);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+getLaptopBrands();
+//Get CPU data from Api
+const getCPU = async function () {
+  try {
+    const res = await fetch("https://pcfy.redberryinternship.ge/api/cpus");
+    const data = await res.json();
+    const laptopCPU = data.data.reverse();
+    laptopCPU.map((cpu) => {
+      renderData(laptopCpuContainer, cpu.id, cpu.name);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+getCPU();
+//Render Function
+const renderData = function (place, id, value) {
+  let data = `
+    <option value=${id}>${value}</option>
+    `;
+  place.insertAdjacentHTML("afterend", data);
+};
